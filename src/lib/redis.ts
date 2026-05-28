488f01752d944583667631d1086415e54ab3fbdd
totoro <woshiydh08@gmail.com>
2026-05-28 11:01:12 +0800

    feat: docker

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,7 @@
 import Redis from 'ioredis'
 import { env } from '../config/env'
 
-export const redis = new Redis(env.REDIS_URL!, {
+export const redis = new Redis(env.REDIS_URL, {
   maxRetriesPerRequest: 1,
 
   tls: {},
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,3 @@
-import dotenv from "dotenv";
-
-// 必须在所有模块导入前加载环境变量
-dotenv.config();
-
 import app from "./app";
 import { env } from "./config/env";
 
